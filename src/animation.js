80cf2a57cf4383b18dae5b9c365fcef8f288303e
BibianaBalBar <balbuenabibi@gmail.com>
2020-12-07 23:25:11 +0000

    faq animations

--- src/animation.js
+++ src/animation.js
@@ -47,6 +47,19 @@ export const fade = {
   },
 };
 
+export const fadeFaq = {
+  hidden: {
+    opacity: 0,    
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 1.8,
+      ease: "easeOut"
+    },
+  },
+};
+
 export const photoAnim = {
   hidden: {
     scale: 1.5,
--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,43 +1,40 @@
 import React from "react";
 import styled from "styled-components";
 import { StyledAbout } from "../styles";
+import Toggle from "./Toggle";
+import { motion, AnimateSharedLayout } from "framer-motion";
+import { fadeFaq } from "../animation";
 
 const FaqSection = () => {
   return (
     <StyledFaq>
       <h2>Any Questions <span>FAQ</span></h2>
-      <div className="question">
-        <h4>How Do I Start?</h4>
-        <div className="answer">        
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, labore.</p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, labore.</p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Payment Methods</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, labore.</p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>What Products Do You Offer?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, labore.</p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+      <AnimateSharedLayout >
+        <Toggle title="How Do I Start?">        
+          <motion.div variants={fadeFaq} className="answer">        
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, labore.</p>
+          </motion.div>   
+        </Toggle>
+        <Toggle title="Daily Schedule">          
+          <motion.div variants={fadeFaq} className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, labore.</p>
+          </motion.div>       
+        </Toggle>
+        <Toggle title="Payment Methods">        
+          <motion.div variants={fadeFaq} className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, labore.</p>
+          </motion.div>       
+        </Toggle>
+        <Toggle title="What Products Do You Offer?">        
+          <motion.div variants={fadeFaq} className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, labore.</p>
+          </motion.div>       
+        </Toggle>
+      </AnimateSharedLayout>
     </StyledFaq>
   );
 };
