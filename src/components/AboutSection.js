164afa07408f251fc0299ee8e7abf8c717de651a
BibianaBalBar <balbuenabibi@gmail.com>
2020-12-07 14:56:30 +0000

    about animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,6 +3,7 @@ import home1 from "../img/home1.png";
 import { StyledAbout, StyledDescription, StyledImg, StyledHide } from "../styles";
 //Framer Motion
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
 
 const AboutSection = () => {
   // const titleAnim = {
@@ -28,29 +29,29 @@ const AboutSection = () => {
       <StyledDescription>
         <motion.div>
           <StyledHide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               We work to make
             </motion.h2> 
           </StyledHide>
           <StyledHide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come 
             </motion.h2>
           </StyledHide>
           <StyledHide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               true.
             </motion.h2>
           </StyledHide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We 
           have professionals with amazing skil.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </StyledDescription>
       <StyledImg>
-        <img src={home1} alt="guy with camera"/>
+        <motion.img variants={photoAnim} src={home1} alt="guy with camera"/>
       </StyledImg>
     </StyledAbout>
   );
